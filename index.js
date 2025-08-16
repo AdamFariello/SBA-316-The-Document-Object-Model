3ea81b2a505a1cc5892bfe2b5d0d115ee660af68
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-08-16 12:56:59 -0400

    Created function to update image opacity

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let topImg = document.getElementById("topImg")
+function updateOpacity () {
+    //topImg.setAttribute("opacity", 0.1)]
+    let topImgStyles = window.getComputedStyle(topImg);
+    const opacity = topImgStyles.getPropertyValue("opacity")
+    console.log(opacity)
+    topImg.style.opacity = opacity - 0.25
+}
